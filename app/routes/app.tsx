bd51cc9ad7daf3244888165bff18699f079dde16
Hammad <hammad@MacBook-Pro.local>
2026-06-03 16:03:23 +0500

    fix: remove in-app sidebar, use Shopify native app navigation

--- app/routes/app.tsx
+++ app/routes/app.tsx
@@ -1,6 +1,6 @@
 import type { HeadersFunction, LoaderFunctionArgs } from "react-router";
-import { Outlet, useLoaderData, useRouteError, useLocation } from "react-router";
-import { AppProvider as PolarisProvider, Frame, Navigation } from "@shopify/polaris";
+import { Outlet, useLoaderData, useRouteError } from "react-router";
+import { AppProvider as PolarisProvider } from "@shopify/polaris";
 import { AppProvider } from "@shopify/shopify-app-react-router/react";
 import { boundary } from "@shopify/shopify-app-react-router/server";
 import { authenticate } from "../shopify.server";
@@ -13,25 +13,11 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
 
 export default function App() {
   const { apiKey } = useLoaderData<typeof loader>();
-  const location = useLocation();
 
   return (
     <AppProvider embedded apiKey={apiKey}>
       <PolarisProvider i18n={en}>
-        <Frame
-          navigation={
-            <Navigation location={location.pathname}>
-              <Navigation.Item label="Dashboard" url="/app" />
-              <Navigation.Item label="Orders" url="/app/orders" />
-              <Navigation.Item label="Analytics" url="/app/analytics" />
-              <Navigation.Item label="COD Rules" url="/app/rules" />
-              <Navigation.Item label="Blacklist" url="/app/blacklist" />
-              <Navigation.Item label="Settings" url="/app/settings" />
-            </Navigation>
-          }
-        >
-          <Outlet />
-        </Frame>
+        <Outlet />
       </PolarisProvider>
     </AppProvider>
   );
